215ce0f4e0d6d110eece79b7e30aad780bd5ddbf
Cassandra Wilcox <cassie@codehangar.io>
2017-10-09 13:04:21 -0400

    feat: create hello world express server

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000!')
+});
