17c5b89dbdcbe9afd8bc21da572cb655b7cdc11a
Cassandra Wilcox <cassie@codehangar.io>
2017-10-10 13:40:41 -0400

    feat: add user schema

--- models/user.js
+++ models/user.js
@@ -1,3 +1,27 @@
-/**
- * Created by cassandrawilcox on 10/10/17.
- */
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  passwordConf: {
+    type: String,
+    required: true,
+  }
+});
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
