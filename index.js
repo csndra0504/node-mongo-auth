2bd8d6d4473390873a344432ea7737b5fe1046ef
Cassandra Wilcox <cassie@codehangar.io>
2017-10-10 15:05:31 -0400

    feat: connect to mongodb

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import routes from './routes/router';
 
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
-});
+// middleware to parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// mount routes middleware to '/' path
+app.use('/', routes);
 
 app.listen(3000, function () {
   console.log('Listening on port 3000!')
 });
 
-// parse incoming requests
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+//connect to MongoDB
+mongoose.connect('mongodb://localhost/testForAuth');
+const db = mongoose.connection;
+
+//handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('connected to mongodb!')
+});
---
+++ routes/router.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import User from  '../models/user';
+
+const router =  express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+router.post('/', (req, res, next) => {
+  // confirm that user typed same password twice
+  if (req.body.password !== req.body.passwordConf) {
+    const err = new Error('Passwords do not match.');
+    err.status = 400;
+    res.send("passwords don't match");
+    return next(err);
+  }
+
+  // create user if all registration fields provided
+  // otherwise return error
+  if (req.body.email &&
+    req.body.username &&
+    req.body.password &&
+    req.body.passwordConf) {
+
+    const userData = {
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password,
+      passwordConf: req.body.passwordConf,
+    };
+
+    User.create(userData, (error, user) => {
+      if (error) {
+        return next(error);
+      } else {
+        req.session.userId = user._id;
+        console.log('success!', user._id);
+      }
+    });
+
+  }  else {
+    const err = new Error('All fields required.');
+    err.status = 400;
+    return next(err);
+  }
+
+});
+
+module.exports = router;
